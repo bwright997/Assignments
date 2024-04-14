cdf6f19626a91aee185df1efe04fce3d12a0a05c
bwright997 <164107321+bwright997@users.noreply.github.com>
2024-04-13 21:38:21 -0500

    Week 5 Project

---
+++ Week5/Week5Project.java
@@ -0,0 +1,15 @@
+package week5;
+
+public class Week5Project {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+	public interface Logger {
+	    // Method for logging messages
+	    void log(String message);
+	}
+
+}
